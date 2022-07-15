9cc3b5c111d805cdb7b7eb013fddb44d56e0fa8a
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2022-07-15 14:41:29 +0200

    add tests

---
+++ src/oata/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package oata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testNothing() {
+    }
+
+    @Test
+    public void testWillAlwaysFail() {
+        fail("My test error message");
+    }
+}
